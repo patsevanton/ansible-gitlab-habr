67115e8481317d569d0cbe935a85426c949994e9
Anton Patsev <patsev.anton@gmail.com>
2018-12-02 21:34:47 +0600

    Update create_ssh_key.sh

--- team/application/create_ssh_key.sh
+++ team/application/create_ssh_key.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh && chmod 700 ~/.ssh
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+eval $(ssh-agent -s)
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+# Old
+#mkdir -p ~/.ssh && chmod 700 ~/.ssh
+#echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
+#chmod 600 ~/.ssh/id_rsa
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_rsa
